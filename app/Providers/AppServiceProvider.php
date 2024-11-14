1eed7c71bd0c209e9dc0072db65e7abea7f70630
fajarikas <fajarika.2021@student.uny.ac.id>
2024-11-14 10:45:20 +0700

    add meta tag on appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Facades\Filament;
+use Illuminate\Support\HtmlString;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        Filament::pushMeta([
+            new HtmlString('<meta name="csrf_token" value="{{ csrf_token() }}"/>'),
+        ]);
     }
 }
