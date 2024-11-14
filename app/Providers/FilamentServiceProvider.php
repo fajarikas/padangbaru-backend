e6b19496595b5d0724ffcd7b297877175024bba5
fajarikas <fajarika.2021@student.uny.ac.id>
2024-11-14 10:52:45 +0700

    add filament service provider

---
+++ app/Providers/FilamentServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Filament\Facades\Filament;
+use Illuminate\Support\ServiceProvider;
+
+class FilamentServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Filament::serving(function () {
+            Filament::registerRenderHook(
+                'head.start',
+                fn() => '<meta name="csrf-token" content="' . csrf_token() . '">'
+            );
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
